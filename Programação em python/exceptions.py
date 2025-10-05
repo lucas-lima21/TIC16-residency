8a9c39b2b1d339966b0658b63fa7f4515ba34806
lucas-lima21 <lucas.lvl.lima@hotmail.com>
2025-10-05 18:10:08 -0300

    Update exception handling example and add PDF
    Modified exceptions.py to use a numeric name and corrected print statements. Added Programaçao python.pdf to the repository.

--- Programação em python/exceptions.py
+++ Programação em python/exceptions.py
@@ -1,10 +1,8 @@
 try:
-    nome = "Lucas Lima"
+    nome = "123"
     for caractere in nome:
         if "0" <= caractere <= "9":
             raise ValueError("O nome nao pode conter números.")
-        print(f"Noem digitado: {nome}")
-except ValueError as e:
-    print(f"Erro: {e}")
+    print(f"Nome digitado: {nome}")
 finally:
-    print("Exemplo completo de utilizacao \ do tratamento de excecoes.")
+    print("Exemplo completo de utilizacao do tratamento de excecoes.")
